909ac97d0a15e6ebcbcdcc7b41c8bde882036ebf
Estiduar <144980889+Estiduar@users.noreply.github.com>
2023-09-19 11:42:08 -0300

    Update mi_pokeapi_app.py

--- mi_pokeapi_app.py
+++ mi_pokeapi_app.py
@@ -76,5 +76,5 @@ def obtener_pokemon_mas_largo(tipo):
         return jsonify({'error': 'No se encontró el tipo de Pokémon'}), 404
 
 if __name__ == "__main__":
-   app.run(host="localhost", port=5000, debug=True)
+   app.run(host="0.0.0.0", port=5000, debug=True)
 
